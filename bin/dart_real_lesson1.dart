f98bbf191fc6d4706bb7be7fc06bf29286e6162f
Amira <kenenbaeva01@gmail.com>
2023-03-27 20:00:43 +0600

    вывели в терминал hello world

---
+++ bin/dart_real_lesson1.dart
@@ -0,0 +1,5 @@
+import 'package:dart_real_lesson1/dart_real_lesson1.dart' as dart_real_lesson1;
+
+void main() {
+  print('Hello world');
+}
---
+++ test/dart_real_lesson1_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_real_lesson1/dart_real_lesson1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
